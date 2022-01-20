374404cf8c89307ee40ac39ff9b0f5a8beb3cf90
kogi-b <briankogi8@gmail.com>
2022-01-20 22:55:46 +0300

    First commit

---
+++ precache-manifest.17324433d92ecf6fc0d2b0f547ccd50e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb48208fff4c564f2a73155ec395c82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0959b36ee770b71f519",
+    "url": "/static/css/main.fb86bce3.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0959b36ee770b71f519",
+    "url": "/static/js/main.f9c65b04.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
